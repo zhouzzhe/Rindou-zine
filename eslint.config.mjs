666c6c4dbf1ddb19e6552757818757a60d424b7a
zhouzzhe <zzhe828@gmail.com>
2025-04-02 16:34:41 +0800

    0402

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 關閉 no-explicit-any 規則，有any型別不會跳錯誤
+      "@typescript-eslint/no-unused-vars": "off", // 關閉 no-explicit-any 規則，沒用到的參數不會跳錯誤
+    },
+  },
 ];
 
 export default eslintConfig;
